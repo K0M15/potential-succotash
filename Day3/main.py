8cdab3d5d966d9738611760bccd4d1d91bdcbf61
Alain Felger <alain.felger93@googlemail.com>
2024-12-03 13:22:43 +0100

    added day 3 part 2

--- Day3/main.py
+++ Day3/main.py
@@ -2,14 +2,23 @@
 import re
 
 def find_pattern(str:str) -> list[str]:
-	return re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", str)
+	return re.findall("(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\))", str)
 
 if __name__ == "__main__":
 	data = find_pattern(open("input.txt").read())
 	result = 0
+	enabled = True
+	print(data)
 	for elem in data:
-		[part1, part2] = elem.split(',', 1)
-		part1 = part1[4:]
-		part2 = part2[:-1]
-		result = result + int(part1)*int(part2)
+		if elem.startswith('mul') and enabled:
+			[part1, part2] = elem.split(',', 1)
+			part1 = part1[4:]
+			part2 = part2[:-1]
+			result = result + int(part1)*int(part2)
+		elif elem.startswith('do()'):
+			enabled = True
+		elif elem.startswith("don't()"):
+			enabled = False
+		else:
+			print(f"Encountered {elem}")
 	print(result)
